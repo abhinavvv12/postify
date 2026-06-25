7c478a689357911fb3a89553f620398b47cdb897
Abhinav <abhinav.r1296@gmail.com>
2026-06-25 11:03:18 +0530

    handled Submit in the create post API

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -6,41 +6,46 @@ const CreatePost = () => {
 
     const navigate = useNavigate()
 
-    const handleClick = async (e) =>{
-        console.log("HANDLER FIRED");
+    const handleClick = async (e) => {
+        e.preventDefault();
 
+        setLoading(true);
 
-        e.preventDefault()
+        const formData = new FormData(e.target);
 
-        console.log("form Submitting")
+        axios.post(`${import.meta.env.VITE_API_URL}/create-post`, formData)
+            .then((res) => {
+                console.log(res);
+                navigate("/feed");
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("Error Creating Post");
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
-        const formData = new FormData(e.target)
+    return (
+        <section className='create-post-container'>
 
-        axios.post(`${import.meta.env.VITE_API_URL}/create-post`, formData).then((res) => {
-            console.log(res)
-            navigate("/feed")
-        })
-        .catch((err)=>{
-            console.log(err)
-            alert("Error Creating Post")
-        })
+            <h1>Create Post</h1>
 
-    }
+            <form onSubmit={handleClick}>
 
-  return (
-    <section className='create-post-container'>
+                <input type="file" name='image' accept='image/*' />
+                <input type="text" name='prompt' placeholder='Enter Caption' />
+                <button
+                    type="submit"
+                    disabled={loading}
+                >
+                    {loading ? "Submitting..." : "Submit"}
+                </button>
 
-        <h1>Create Post</h1>
+            </form>
 
-        <form onSubmit={handleClick}>
-
-            <input type="file"  name='image' accept='image/*' />
-            <input type="text" name='prompt' placeholder='Enter Caption' />
-            <button type='submit' >Submit</button>
-
-        </form>
-
-    </section>
-  )
+        </section>
+    )
 }
 export default CreatePost
